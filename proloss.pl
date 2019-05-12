8fb3bb8d0d01073abfac14fcb0e21e15efe964c6
Artur Juraszek <artur@juraszek.xyz>
2019-05-12 22:31:25 +0200

    Define a grammar which will be used to parse the maps loaded

--- proloss.pl
+++ proloss.pl
@@ -8,8 +8,32 @@ process_words_file(Filename) :-
     atom_chars(WordAsAtom, WordAsLetterList),
     assert(word(WordAsLetterList)),
     IsItEnd == -1,
-    close(Descriptor).
+    close(Descriptor),
+    !.
+
+letter(T) -->
+    [?], { length(T, 1) }, !.
+letter([Letter]) -->
+    [Letter], !.
+
+word([]) -->
+    [].
+word([■]) -->
+    [■].
+word([Letter|Tail]) -->
+    letter(Letter), word(Tail).
+
+%% ?- phrase(letter(Output), X).
+%% ?- phrase(letter(Output), [a]).
+%% ?- phrase(word(Output), [a,b,c,■,?,f,?,?]).
+%@ Output = [[a], [b], [c], [■], [_4772], [f], [_4796], [_4808]] ;
+
 
 %% ?- working_directory(_, '/home/asgavar/').
 %% ?- process_words_file('samples/input/proloss-words-pl').
 %% ?- listing.
+
+%% - funkcja parsująca jeden wiersz - atom -> atom, ? -> length(T, 1)
+%% [a,b,?,?,d] -> [a,b,_G1,_G2,d]
+%% - funkcja-wrapper budująca [H|T] z kolejnych wierszy
+%% - właściwy algos na tym, co wyjdzie z powyższych
